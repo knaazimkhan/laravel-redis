b411166eef66ff2764396471b5a8014cc69a0067
knaazimkhan <knaazimkhan@gmail.com>
2019-04-28 17:12:21 +0530

    redis install tested and working

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\User;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +16,19 @@
 */
 
 Route::get('/', function () {
+    // DB::connection()->enableQueryLog();
+    // $users_cache = Cache::remember('users', 1, function () {
+    //     return User::all()->take(5);
+    // });
+    // $log = DB::getQueryLog();
+    // print_r($log);
+    // return $users_cache;
     return view('welcome');
+    // $redis = app()->make('redis');
+    // $redis->set('name', 'Naazim Khan');
+    // return $redis->get('name');
+    // $id = 1;
+    // $storage = Redis::connection();
+    // $view = $storage->incr('viewCount:' .$id. ':views' );
+    // return 'This is an artical with id: '.$id. ' it has '.$view. ' view';
 });
